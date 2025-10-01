c7b5259af75de3630d12c5575b3d66505a81b874
Shifa Tazeen Shaikh <shifatazeenshaikh@Mac.lan>
2025-10-01 11:57:34 +0530

    Vite url

--- frontend/quotation-ui/src/api.jsx
+++ frontend/quotation-ui/src/api.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000', // Backend URL
+  baseURL: 
+  import.meta.env.VITE_API_URL,
 });
 
 export const setAuthToken = (token) => {
